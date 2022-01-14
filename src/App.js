6c970c113929373c5b887433a3d13ffb94532120
Shiv <pals55002@gmail.com>
2022-01-14 11:32:51 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Carousel from "./components/Carousel";
+import { Footer } from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <Navbar />
      <Carousel />
+     <Footer/>
     </div>
   );
 }
